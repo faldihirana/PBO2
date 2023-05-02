df7d525b07c2e17398c34ef78720ec9d11d5ce35
Faldi Hirana _ 190511011 <127743319+faldihirana@users.noreply.github.com>
2023-05-02 13:48:42 +0700

    Add files via upload

---
+++ PBO2/Praktikum6/concurrentfutures.py
@@ -0,0 +1,12 @@
+import concurrent.futures
+
+def worker(num):
+    """Fungsi untuk menjalankan tugas pada proses baru"""
+    print('Worker', num)
+
+if __name__ == '__main__':
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+
+        for i in range(5):
+            executor.submit(worker, i)
